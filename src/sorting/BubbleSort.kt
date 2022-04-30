6bbe2db82c9991145e9619b6eb69f7a92edf27a8
신승혁 <Cozak92@gmail.com>
2022-04-30 13:18:27 +0900

    거품정렬 구현

--- src/sorting/BubbleSort.kt
+++ src/sorting/BubbleSort.kt
@@ -1,7 +1,25 @@
 package sorting
 
-class BubbleSort:SoringAlgorithm<MutableList<Int>> {
+import extention.swap
+
+class BubbleSort : SoringAlgorithm<MutableList<Int>> {
+    init {
+        println("Insertion Sort is Starting \n" +
+                "※ Worst case performance O(n^2) \n" +
+                "※ Best case performance O(n) \n" +
+                "※ Average case performance O(n^2)")
+
+    }
     override fun sort(arr: MutableList<Int>) {
-        TODO("Not yet implemented")
+        arr.forEachIndexed { index, i ->
+            for (j in 1 until arr.size - index) {
+                if (arr[j] < arr[j - 1]) {
+                    arr.swap(j, j - 1)
+                }
+            }
+        }
+
+        println(arr.toString())
+
     }
 }
